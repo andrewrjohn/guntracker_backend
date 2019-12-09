4bceb67e48e0222ca07cc489c485dbfc5d57a166
Andrew Johnson <andrew.johnson@sportshubtech.com>
2019-12-09 15:28:00 -0600

    Default port

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const routers = require('./routers')
+const express = require('express');
+const routers = require('./routers');
 const bodyParser = require('body-parser');
-require('dotenv').config()
-require('./db')
-require('./models')
+require('dotenv').config();
+require('./db');
+require('./models');
 
-const app = express()
-const { PORT } = process.env
+const app = express();
+const { PORT } = process.env;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use('/api/v1', routers)
+app.use('/api/v1', routers);
 
-app.listen(PORT, () => console.log(`API server listening on port ${PORT} 🎉`))
+app.listen(PORT || 3000, () => console.log(`API server listening on port ${PORT || 3000} 🎉`));
